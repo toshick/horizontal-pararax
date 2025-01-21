6049821ab192fe8b6a3297aa23f062335d8eedbf
toshick <toshickjazz@mac.com>
2025-01-21 16:24:25 +0900

    feat: set ease

--- apps/sample01/src/setup.ts
+++ apps/sample01/src/setup.ts
@@ -10,15 +10,15 @@ export function setupGsap() {
   if (listEl && listWrapperEl) {
     gsap.to(listEl, {
       x: () => -(listEl.clientWidth - listWrapperEl.clientWidth),
-      ease: 'none',
+      ease: 'ease',
       scrollTrigger: {
         trigger: '.SideScroll',
         start: 'top top', // 要素の上端（top）が、ビューポートの上端（top）にきた時
         end: () => `+=${listEl.clientWidth - listWrapperEl.clientWidth}`,
         scrub: true, //スクロールにあわせてアニメーションさせる
         pin: true, //縦スクロールが停止して画面が固定される
-        anticipatePin: 1,
-        invalidateOnRefresh: true,
+        anticipatePin: 0, //固定のタイミングをうまく検知する?
+        invalidateOnRefresh: true, //リサイズ時の再計算
       },
     });
   }
